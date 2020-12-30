7bce285647068defbf124acf00b6b4353a513227
YorKeN <dethoor.axel@gmail.com>
2020-12-30 12:17:16 +0100

    app done

---
+++ src/convertisseur/ConvertisseurRGBToHexa.java
@@ -0,0 +1,37 @@
+package convertisseur;
+
+import java.util.Scanner;
+
+public class ConvertisseurRGBToHexa {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("---- Convertisseur RGB Color To Hexadecimal ---- ");
+		System.out.println("R:");
+		int r = in.nextInt();
+		System.out.println("G:");
+        int g = in.nextInt();
+        System.out.println("B:");
+        int b = in.nextInt();
+        String R = Integer.toHexString(r).toUpperCase();
+        String G = Integer.toHexString(g).toUpperCase();
+        String B = Integer.toHexString(b).toUpperCase();
+        if(R.equals("0")) {
+        	R = "00";
+        }else if(r < 10) {
+        	R = "0" + R;
+        }
+        if(G.equals("0")) {
+        	G = "00";
+        }else if(g < 10) {
+        	G = "0" + G;
+        }
+        if(B.equals("0")) {
+        	B = "00";
+        }else if(b < 10) {
+        	B = "0" + B;
+        }
+        String hexaColor = "#"+R+G+B;
+        System.out.println("("+ r + ", " + g + ", " + b + ") = " + hexaColor + " en Hexadécimal");
+	}
+
+}
